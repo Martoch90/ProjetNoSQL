fc8293711f27d3e9ab94f50fa2a4784c70392828
Martin Fouques <martinfouques@gmail.com>
2023-12-06 18:19:13 +0100

    Barre de recherche

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from flask import jsonify
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
@@ -26,10 +27,21 @@ def nouvel_article():
     return render_template('nouvel_article.html')
 
 # Page de recherche d'articles
-@app.route('/recherche')
+@app.route('/recherche', methods=['GET', 'POST'])
 def recherche():
-    articles = collection.find()
-    return render_template('recherche.html', articles=articles)
+    if request.method == 'POST':
+        # Obtenez le terme de recherche à partir du formulaire
+        terme_recherche = request.form['terme_recherche']
+
+        # Effectuez la recherche dans la base de données
+        articles = collection.find({'designation': {'$regex': terme_recherche, '$options': 'i'}})
+        
+        # Convertissez les résultats en liste pour l'affichage
+        articles_list = list(articles)
+
+        return render_template('recherche.html', articles=articles_list, terme_recherche=terme_recherche)
+
+    return render_template('recherche.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
