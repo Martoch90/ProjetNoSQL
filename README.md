d5f69091c77df421457b293e21d40666e2f7aaf9
Martin Fouques <martinfouques@gmail.com>
2024-01-05 11:26:29 +0100

    Supression Doublons import et maj des commentaires

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Sur le terminal unbuntu :
 sudo service redis-server start
 
 Lancer MongoDB avec mongodb://localhost:27017/
-Si besoin de modifier les id du connxion du base de donnée elle se trouve dans le fichier app.py ligne 14 à 20
+Si besoin de modifier les id du connxion du base de donnée elle se trouve dans le fichier app.py ligne 10 à 15
 
 # Pour lancer le programme
 Sur le terminal :
--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from pymongo import MongoClient
-from flask import jsonify
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
 from pymongo import MongoClient
 import redis
 import json
-from bson import json_util
-from bson import ObjectId
+from bson import json_util, ObjectId
 import csv
 import io
 
@@ -18,54 +14,48 @@ db = client['gestion_stock']
 collection = db['articles']
 redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
 
-@app.route('/')
+@app.route('/') #Page d'accueil
 def index():
     return render_template('index.html')
 
-@app.route('/nouvel_article', methods=['GET', 'POST'])
+@app.route('/nouvel_article', methods=['GET', 'POST']) #Page d'ajout d'un nouvel article
 def nouvel_article():
-    message = None  # Initialisez le message à None
+    message = None
 
     if request.method == 'POST':
         designation = request.form['designation']
         prix_unitaire = float(request.form['prix_unitaire'])
 
-        collection.insert_one({'designation': designation, 'prix_unitaire': prix_unitaire})
+        collection.insert_one({'designation': designation, 'prix_unitaire': prix_unitaire}) #Ajout de l'article dans la base de données
 
-        message = 'Ajoute ' + designation + ' ' + str(prix_unitaire) + ' avec succès'
+        message = 'Ajoute ' + designation + ' ' + str(prix_unitaire) + ' avec succès' #Message de confirmation
 
     return render_template('nouvel_article.html', message=message)
 
-@app.route('/ajout_de_donnees', methods=['GET', 'POST'])
+@app.route('/ajout_de_donnees', methods=['GET', 'POST']) #Page d'ajout de données
 def ajout_de_donnees():
-    if request.method == 'POST':
-        # Vérifiez si un fichier a été téléchargé
+    if request.method == 'POST': #Vérification du fichier existant ou le fichier non approprié
         if 'file' not in request.files:
             flash('Aucun fichier n\'a été téléchargé.', 'error')
             return redirect(request.url)
 
         file = request.files['file']
 
-        # Vérifiez si le fichier a un nom
         if file.filename == '':
             flash('Aucun fichier sélectionné.', 'error')
             return redirect(request.url)
 
-        # Vérifiez si le fichier est un fichier texte
-        if file and file.filename.endswith('.txt'):
+        if file and file.filename.endswith('.txt'): #Vérification du format du fichier
             try:
-                # Ouvrir le fichier en mode texte avec io.TextIOWrapper
                 text_file = io.TextIOWrapper(file, encoding='utf-8', newline='')
                 
-                # Lecture du fichier texte CSV en mode texte
                 csv_data = csv.reader(text_file, delimiter=';')
 
                 for row in csv_data:
                     designation, prix_unitaire = row
                     prix_unitaire = float(prix_unitaire)
 
-                    # Insérez les données dans la base de données
-                    collection.insert_one({'designation': designation, 'prix_unitaire': prix_unitaire})
+                    collection.insert_one({'designation': designation, 'prix_unitaire': prix_unitaire}) #Ajout de l'article dans la base de données
 
                 flash('Les données ont été ajoutées avec succès.', 'success')
             except Exception as e:
@@ -76,14 +66,13 @@ def ajout_de_donnees():
 
     return render_template('ajout_de_donnees.html')
 
-@app.route('/recherche', methods=['GET', 'POST'])
+@app.route('/recherche', methods=['GET', 'POST']) #Page de recherche
 def recherche():
     if request.method == 'POST':
         terme_recherche = request.form['terme_recherche']
         
         cached_results = redis_client.get(terme_recherche)
         if cached_results:
-            # Chargez les résultats depuis Redis et convertissez les ObjectId
             articles = json.loads(cached_results, object_hook=object_hook)
         else:
             # Effectuez la recherche dans la base de données
@@ -102,22 +91,18 @@ def recherche():
 
     return render_template('recherche.html')
 
-# Nouvelle route pour la suppression par nom
-@app.route('/suppression_par_nom', methods=['GET', 'POST'])
+
+@app.route('/suppression_par_nom', methods=['GET', 'POST']) #Page de suppression par nom
 def suppression_par_nom():
     if request.method == 'POST':
         nom_a_supprimer = request.form['nom_a_supprimer']
-        
-        # Vérifiez si l'article avec le nom spécifié existe dans la base de données
-        article = collection.find_one({'designation': nom_a_supprimer})
+        article = collection.find_one({'designation': nom_a_supprimer}) #Recherche de l'article dans la base de données
         
         if article:
-            # Si l'article existe, supprimez-le de la base de données
-            collection.delete_one({'designation': nom_a_supprimer})
+            collection.delete_one({'designation': nom_a_supprimer}) #Suppression de l'article dans la base de données
             redis_client.flushall()
             message = f"L'article '{nom_a_supprimer}' a été supprimé avec succès."
         else:
-            # Si l'article n'existe pas, affichez un message approprié
             message = f"L'article '{nom_a_supprimer}' n'existe pas."
 
         return render_template('suppression_par_nom.html', message=message)
@@ -125,8 +110,7 @@ def suppression_par_nom():
     return render_template('suppression_par_nom.html')
 
     
-# Fonction de conversion pour ObjectId
-def object_hook(dct):
+def object_hook(dct): #Fonction de conversion ObjectId
     for key, value in dct.items():
         if isinstance(value, dict):
             dct[key] = object_hook(value)
@@ -134,6 +118,6 @@ def object_hook(dct):
             dct[key] = ObjectId(value["$oid"])
     return dct
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Lancement de l'application
     app.run(debug=True)
 
